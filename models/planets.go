5f90a40b9ab62dca72ddd6cb202d15e4800f206d
David <david@david.luna>
2020-11-26 11:14:05 -0300

    chore: Creating planets model

---
+++ models/planets.go
@@ -0,0 +1,11 @@
+package models
+
+import "gopkg.in/mgo.v2/bson"
+
+//Planet model
+type Planet struct {
+	ID      bson.ObjectId `bson:"_id" json:"id"`
+	Name    string        `bson:"name" json:"name"`
+	Climate string        `bson:"climate" json:"climate"`
+	Terrain string        `bson:"terrain" json:"terrain"`
+}
