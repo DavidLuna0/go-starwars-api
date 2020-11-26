68334a1e2f07ee8b1025220dac74aae8fdbd01aa
David <david@david.luna>
2020-11-26 17:05:20 -0300

    chore: Creating routes and server config

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	. "github.com/star-wars-go/config"
+	. "github.com/star-wars-go/dao"
+	planetRouter "github.com/star-wars-go/router"
+)
+
+var dao = PlanetsDAO{}
+var config = Config{}
+
+func init() {
+	config.Read()
+
+	dao.Server = config.Server
+	dao.Database = config.Database
+	dao.Connect()
+}
 
 func main() {
-	fmt.Println("First commit")
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/planets", planetRouter.GetAll).Methods("GET")
+	r.HandleFunc("/api/v1/planets/{id}", planetRouter.GetByID).Methods("GET")
+	r.HandleFunc("/api/v1/planets", planetRouter.Create).Methods("POST")
+	r.HandleFunc("/api/v1/planets/{id}", planetRouter.Update).Methods("PUT")
+	r.HandleFunc("/api/v1/planets/{id}", planetRouter.Delete).Methods("DELETE")
+
+	var port = ":3000"
+	fmt.Println("Server running in port: ", port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
